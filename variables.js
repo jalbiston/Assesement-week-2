af3e1ca4771d1699dc5712b151c194cd8c359f24
jalbiston <jacob.albiston@gmail.com>
2021-08-28 18:03:44 -0600

    for loop

--- variables.js
+++ variables.js
@@ -30,5 +30,6 @@ if (faveNum === 13) {
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
-for (let faveNum; faveNum < 5; faveNum++) {
+for (let i = 0; i < 5; i++) {
+  console.log(faveNum)
 }
